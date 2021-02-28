182986a90d0d9427f92272b4665662f8e9c663cb
JErBerlin <jeronirm@gmail.com>
2021-02-28 12:44:25 +0100

    game table, types and func; passing tests

---
+++ game.go
@@ -0,0 +1,69 @@
+package main
+
+// Move codes the possible throws or moves of the game
+type Move int
+
+const (
+	Rock Move = iota
+	Paper
+	Scissors
+	MovesLength
+)
+
+// String returns the string labels of enum ints
+func (m Move) String() string {
+	switch m {
+	case Rock:
+		return "rock"
+	case Paper:
+		return "paper"
+	case Scissors:
+		return "scissors"
+	}
+	return ""
+}
+
+// Player codes the id of the players
+type Player int
+
+const (
+	Human Player = iota
+	Computer
+)
+
+// Outcome codes lose, draw and win
+type Outcome int
+
+const (
+	Lose Outcome = iota - 1
+	Draw
+	Win
+)
+
+// String returns the string labels of enum ints
+func (o Outcome) String() string {
+	switch o {
+	case Lose:
+		return "lose"
+	case Draw:
+		return "draw"
+	case Win:
+		return "win"
+	}
+	return ""
+}
+
+// Outcome table tells you if player one will lose, draw or win against player two
+// given moves of player one and two. We write the whole matrix although not needed.
+// (by design of the game, it is an anti-symmetric, hollow matrix)
+var OutcomeTable = [][]Outcome{
+	{Draw, Lose, Win},
+	{Win, Draw, Lose},
+	{Lose, Win, Draw},
+}
+
+// Game returns if Player one will lose, draw or win against player two given
+// the moves of player one and two by looking it up in the OutcomeTable
+func Game(playerOneMove, playerTwoMove Move) Outcome {
+	return OutcomeTable[playerOneMove][playerTwoMove]
+}
---
+++ game_test.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestGame(t *testing.T) {
+	var tests = []struct {
+		desc         string
+		humanMove    Move
+		computerMove Move
+		want         Outcome
+	}{
+		{"rock on rock", Rock, Rock, Draw},
+		{"paper on paper", Paper, Paper, Draw},
+		{"scissors on scissors", Scissors, Scissors, Draw},
+		{"rock on paper", Rock, Paper, Lose},
+		{"rock on scissors", Rock, Scissors, Win},
+		{"paper on rock", Paper, Rock, Win},
+		{"paper on scissors", Paper, Scissors, Lose},
+		{"scissors on rock", Scissors, Rock, Lose},
+		{"scissors on paper", Scissors, Paper, Win},
+	}
+	for _, test := range tests {
+		fmt.Printf("Test run: %v\n", test)
+		got := Game(test.humanMove, test.computerMove)
+		if got != test.want {
+			t.Errorf("%s: %v on %v = %v, want %v", test.desc, test.humanMove, test.computerMove, got, test.want)
+		}
+	}
+}
