6bd32d7b7bc6b0ad8bda19fa4898020ce4380dd8
JerBerlin <jeronir@hotmail.com>
2023-06-29 11:38:23 +0700

    adapted w/o well gameAI, no strategy

--- game/gameAI.go
+++ game/gameAI.go
@@ -1,3 +1,4 @@
+// gameAI.go
 // gameAI provides a way for the computer to choose a move according to a strategy
 // You need to do an initialization of the random seed before using gameAI
 package game
@@ -6,10 +7,8 @@ import (
 	"math/rand"
 )
 
-// ComputerChooseMove returns a randomly generated move according
-// a given strategy
+// ComputerChooseMove returns a randomly generated move.
+// Each move is equally likely to be chosen.
 func ComputerChooseMove() Move {
-	// mixed strategy: equiprobable of strong elements, never choose rock
-	// observe that we choose moves randomly from second element to the last
-	return Move(1+rand.Intn(int(MovesLength)-1))
+	return Move(rand.Intn(int(MovesLength)))
 }
