3b2a054d4d3a5ce40309be0b1b281e6776582549
JerBerlin <jeronir@hotmail.com>
2023-06-29 11:56:03 +0700

    adapt w/o well game tests

--- game/game_test.go
+++ game/game_test.go
@@ -1,7 +1,6 @@
 package game
 
 import (
-	"fmt"
 	"testing"
 )
 
@@ -11,29 +10,20 @@ func TestGame(t *testing.T) {
 		humanMove    Move
 		computerMove Move
 		want         Outcome
-	}{ // we can not assume commutativity since we defined the rules in a complete matrix
+	}{
 		{"rock on rock", Rock, Rock, Draw},
 		{"paper on paper", Paper, Paper, Draw},
 		{"scissors on scissors", Scissors, Scissors, Draw},
-		{"well on well", Well, Well, Draw},
 		{"rock on paper", Rock, Paper, Lose},
 		{"rock on scissors", Rock, Scissors, Win},
-		{"rock on well", Rock, Well, Lose},
-		{"paper on rock", Paper, Rock, Win},
 		{"paper on scissors", Paper, Scissors, Lose},
-		{"paper on well", Paper, Well, Win},
-		{"scissors on rock", Scissors, Rock, Lose},
-		{"scissors on paper", Scissors, Paper, Win},
-		{"scissors on well", Scissors, Well, Lose},
-		{"well on rock", Well, Rock, Win},
-		{"well on paper", Well, Paper, Lose},
-		{"well on scissors", Well, Scissors, Win},
 	}
 	for _, test := range tests {
-		fmt.Printf("Test run: %v\n", test)
-		got := Game(test.humanMove, test.computerMove)
-		if got != test.want {
-			t.Errorf("%s: %v on %v = %v, want %v", test.desc, test.humanMove, test.computerMove, got, test.want)
-		}
+		t.Run(test.desc, func(t *testing.T) {
+			got := Game(test.humanMove, test.computerMove)
+			if got != test.want {
+				t.Errorf("%v on %v = %v, want %v", test.humanMove, test.computerMove, got, test.want)
+			}
+		})
 	}
 }
